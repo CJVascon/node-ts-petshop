03542074db92390150eb3225c8ca65803d1c957c
CJVascon <caiovasconcelos.contato@gmail.com>
2022-11-15 10:13:33 -0300

    base do projeto

---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import * as PageController from '../controllers/pageController';
+import * as SearchController from '../controllers/searchController';
+
+const route = Router();
+
+route.get('/', PageController.home);
+route.get('/dogs', PageController.dogs);
+route.get('/cats', PageController.cats);
+route.get('/fishes', PageController.fishes);
+
+route.get('/search', SearchController.search);
+
+export default route;
